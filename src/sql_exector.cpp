5227d4c95d0f32f6ce3c816273fa7d25a202109b
tempertan <tanpengol@hotmail.com>
2020-10-25 15:50:11 +0800

    rename file

---
+++ src/sql_exector.cpp
@@ -0,0 +1,12 @@
+
+#include <string>
+#include <list>
+
+int exec_sql_list( const std::list<std::string>& sqllist )
+{
+    for( std::list<std::string>::const_iterator it = sqllist.cbegin(); it != sqllist.cend(); ++it)
+    {
+        
+    }
+}
+
---
+++ src/sql_loader.cpp
@@ -0,0 +1,27 @@
+#include <list>
+#include <string>
+#include <fstream>
+
+int load_sql_from_file(const std::string path, std::list<std::string> sqls)
+{
+    sqls.clear();
+
+    std::ifstream sqlfile(path);
+    if( !sqlfile )
+    {
+        return -1;
+    } else
+    {
+        std::string sqlstr;
+        while( std::getline(sqlfile,sqlstr) )
+        {
+            if( !sqlstr.empty() )
+            {
+                sqls.push_back(sqlstr);
+            }
+        }
+    }
+    
+}
+
+int 
